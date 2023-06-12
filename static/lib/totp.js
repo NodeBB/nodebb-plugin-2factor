18366a6028e067863f4db76829736182f37c9b0d
Julian Lam <julian@nodebb.org>
2023-06-12 11:10:07 -0400

    fix: disable submit button when auto-submitting

--- static/lib/totp.js
+++ static/lib/totp.js
@@ -6,12 +6,14 @@ define('forum/login-totp', [], function () {
 	Plugin.init = async () => {
 		const codeEl = document.getElementById('code');
 		const formEl = document.querySelector('#content form');
+		const submitBtn = formEl.querySelector('button');
 		if (codeEl) {
 			codeEl.focus();
 
 			codeEl.addEventListener('keyup', (e) => {
 				const length = e.target.value.length;
 				if (length === 6) {
+					submitBtn.disabled = true;
 					formEl.submit();
 				}
 			});
